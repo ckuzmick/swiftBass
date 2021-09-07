cf22def4e336f0fec49b50cca1992b6c9ac67665
ckuzmick <conrad.kuzmick@icloud.com>
2021-09-07 14:52:59 -0400

    Initial Commit

---
+++ swiftBass/swiftBassApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftBassApp.swift
+//  swiftBass
+//
+//  Created by ck on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftBassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
